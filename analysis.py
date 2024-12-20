1415b60856d82faec040b67468a661d0358be461
pawangammaedge <pawan.dabi@gammaedge.io>
2024-12-20 15:53:08 +0530

    new features are added

--- analysis.py
+++ analysis.py
@@ -4,3 +4,14 @@ poke = pd.read_csv('pokemon_data.csv')
 
 print(poke.tail(5))
 
+# print(poke.tail(5))
+# print(poke.describe())
+# print(poke.info())
+numeric_data = ['int64']
+cateogric_data = ['object']
+numeric_columns = poke.select_dtypes(include=numeric_data)
+categoric_columns = poke.select_dtypes(include=cateogric_data)
+# print(numeric_columns)
+# print(numeric_columns.shape)
+print(categoric_columns)
+print(categoric_columns.shape)
